70eb8b071868a319a5507fd562efbb88ac8d6813
omers <omer3026@gmail.com>
2018-06-28 07:07:22 +0300

    wikitube project

---
+++ js/main.js
@@ -0,0 +1,143 @@
+'use strict';
+
+var gSearchStr = '';
+
+
+function init() {
+
+    getSearches();
+    onSearch('eyal golan');
+}
+
+function getSearches() {
+    var searches = getSearchesToDisplay();
+    if (searches) {
+        renderSearches(searches);
+    }
+}
+
+function onSearch(txtSearch) {
+
+    var elInput = document.querySelector('.search-input')
+    txtSearch = !txtSearch ? elInput.value : txtSearch;
+    if (txtSearch.length > 0 || txtSearch !== null) {
+        gSearchStr = txtSearch;
+        askYoutubeLink(txtSearch);
+        saveSearches(txtSearch);
+        getSearches();
+    }
+
+}
+
+function renderResulst(searchRes) {
+
+    var elUl = document.querySelector('.result-yt');
+    var strHtml = '';
+    var selectedSrc = '';
+    var searchResults = searchRes.items;
+
+    searchResults.forEach((ytItem, idx) => {
+        if (idx === 0) selectedSrc = ytItem.id.videoId;
+        strHtml += `
+    <li onclick="onVideoSelected('${ytItem.id.videoId}')">
+    <div class="res-item flex align-center space-between" >
+  <img src="${ytItem.snippet.thumbnails.default.url}" 
+  alt="img" >
+    <p>${ytItem.snippet.title}</p>
+    <div>
+    </li>`
+    });
+
+    onVideoSelected(selectedSrc);
+    askItemWiki(gSearchStr);
+    
+    elUl.innerHTML = strHtml;
+}
+
+function onkeyDown(el,ev){
+    if (ev.code === 'NumpadEnter'){
+        onSearch();
+    }
+}
+
+function onVideoSelected(ytId) {
+    var elFrameSelected = document.querySelector('.selected-video iframe');
+    var elLoad = document.querySelector('.loading');
+    var elTop = document.querySelector('.top-result')
+    elTop.scrollIntoView();
+
+    if (ytId) {
+        elFrameSelected.src = `https://www.youtube.com/embed/${ytId}`;
+        askItemWiki(gSearchStr);
+        setTimeout(function(){
+            elLoad.classList.add('hide')},550)
+    }
+    
+    
+}
+
+function renderTxtWiki(wikiData) {
+    var elWikiData = document.querySelector('.wiki-data');
+    var Heaedr = wikiData[0];
+    var dataLen = wikiData[2].length;
+    var wikiHomeLink = 'https://he.wikipedia.org/wiki/%D7%A2%D7%9E%D7%95%D7%93_%D7%A8%D7%90%D7%A9%D7%99'
+    var strHtml = `<li><h1>${Heaedr}</h1></li>`;
+
+    var subTxt = wikiData[1];
+    var mainTxt = wikiData[2];
+    var txtLinks = wikiData[3];
+    strHtml += subTxt;
+
+    for (var i = 0; i < dataLen; i++) {
+        var currSubLine = subTxt[i];
+        var currMainLine = mainTxt[i];
+        var currLinkLine = txtLinks[i];
+        if (currLinkLine === '') currLinkLine = wikiHomeLink;
+        if (currMainLine === '') currMainLine = 'No details about your serch';
+
+        strHtml += `<li class="wiki-data-item">
+        <h4>${currSubLine}</h4>
+        <p>${currMainLine}</p>
+        <button onclick="onReadMore('${currLinkLine}')" class="btn">Read More</button>
+        </li>`
+
+    }
+
+    elWikiData.innerHTML = strHtml;
+}
+
+function onReadMore(href) {
+    window.open(href, '_blank')
+}
+
+function renderSearches(searches) {
+
+    var elSearches = document.querySelector('.search-list-container ul');
+    var strHtml = '';
+
+    searches.forEach((search, idx) => {
+        strHtml += `
+    <li>
+    <div class="flex align-center space-between" >
+        ${search}
+    <div>
+    </li>`
+    });
+    elSearches.innerHTML = strHtml;
+}
+
+
+function onClear() {
+    confirmClearHistory();
+}
+
+function onChangeColor() {
+    setColor();
+}
+
+function renderColor(colorInput) {
+    var html = document.getElementsByTagName('html')[0];
+    html.style.setProperty("--body-bg-color", colorInput);
+    html.style.setProperty("--el-color", colorInput);
+    var elInput = document.querySelector('.input-color');
+}
---
+++ js/wikiTube-service.js
@@ -0,0 +1,129 @@
+'use strict';
+var YT_KEY = `AIzaSyANrISFcbmdjmL0lrQBylPpUUJmI_dTLk4`
+var SEARCH_KEY = 'searchKey';
+var gSearch = [];
+console.log('servics');
+
+
+function initSearch() {
+    var searches = [];
+    searches = loadFromStorage(SEARCH_KEY);
+
+    if (!searches || searches.length === 0) {
+        searches = ['Your history is clean'];
+    }
+    return searches;
+}
+
+
+function askYoutubeLink(searchStr) {
+    var http = `https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=${YT_KEY}&q=${searchStr}`;
+    console.log(http)
+    var prm = axios.get(http);
+    prm.then(function (res) {
+        renderResulst(res.data);
+    });
+    prm.catch(function (err) {
+        console.error('Had Problem:', err)
+    });
+}
+
+
+function askItemWiki(searchStr) {
+    var http = `https://en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=${searchStr}&limit=5`;
+    console.log(http)
+    var prm = axios.get(http);
+    prm.then(function (res) {
+        console.log(res);
+        renderTxtWiki(res.data);
+    });
+    prm.catch(function (err) {
+        console.error('Had Problem:', err)
+    });
+}
+
+
+
+function saveSearches(txtSearch) {
+    gSearch.push(txtSearch);
+    gSearch = new Set(gSearch);
+    gSearch = Array.from(gSearch);
+    saveToStorage(SEARCH_KEY, gSearch);
+
+}
+
+function getSearchesToDisplay() {
+    gSearch = initSearch();
+    return gSearch;
+}
+
+function clearLocalStorage() {
+    localStorage.removeItem(SEARCH_KEY);
+    gSearch = [];
+}
+
+
+function confirmClearHistory() {
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover this imaginary file!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    })
+        .then((willDelete) => {
+            if (willDelete) {
+                swal("Your history of searches is deleted!", {
+                    icon: "success",
+                });
+                clearLocalStorage();
+                renderSearches(gSearch);
+            } else {
+                swal("Your history of searches is safe!");
+            }
+        });
+
+}
+
+
+function setColor() {
+    var bodyStyles = window.getComputedStyle(document.body);
+    var currVal = bodyStyles.getPropertyValue('--body-bg-color');
+
+    swal("Choose color", {
+        content: {
+            element: "input",
+            attributes: {
+                className: "input-color",
+                placeholder: "Change theme color",
+                value: currVal,
+                type: "color",
+            },
+        },
+        buttons: {
+            cancel: true,
+            confirm: true,
+            Reset: {
+                    value:'#ffca7c'
+            }
+        },
+
+    })
+        .then((value) => {
+            if (value) {
+                renderColor(value)
+            } else {
+                renderColor(currVal)
+            }
+
+        });
+}
+
+
+function saveToStorage(key, value) {
+    localStorage.setItem(key, JSON.stringify(value))
+}
+
+function loadFromStorage(key) {
+    return JSON.parse(localStorage.getItem(key))
+}
